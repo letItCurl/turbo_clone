eba9d460fe4bb79aa6550c8cc80b45e0069ec666
letItCurl <rolandlopez.developer@gmail.com>
2024-05-05 17:17:22 +0700

    Modulize, and create helper

--- app/helpers/turbo_clone/streams_helper.rb
+++ app/helpers/turbo_clone/streams_helper.rb
@@ -3,4 +3,11 @@ module TurboClone::StreamsHelper
     # self == view context
     TurboClone::Streams::TagBuilder.new(self)
   end
+
+  def turbo_stream_from(*streamables, **attributes)
+    attributes[:channel] = "TurboClone::StreamsChannel"
+    attributes["signed-stream-name"] = TurboClone::StreamsChannel.stream_name_from(streamables)
+
+    tag.turbo_cable_stream_source(**attributes)
+  end
 end
