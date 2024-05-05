a9c630e737e05bd2c1d7d023a822f9acbe08546e
letItCurl <rolandlopez.developer@gmail.com>
2024-05-05 12:18:25 +0700

    testing render tag + Delegat + monkey patching

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,3 +13,15 @@ if ActiveSupport::TestCase.respond_to?(:fixture_paths=)
   ActiveSupport::TestCase.file_fixture_path = File.expand_path("fixtures", __dir__) + "/files"
   ActiveSupport::TestCase.fixtures :all
 end
+
+
+# Delegat + monkey patching
+module ActionViewTestCaseExtensions
+  def render(*arguments, **options, &block)
+    ApplicationController.render(*arguments, **options, &block)
+  end
+end
+
+class ActionDispatch::IntegrationTest
+  include ActionViewTestCaseExtensions
+end
