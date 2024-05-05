44e103ec65fa9cc67ae813972805e2a68e15632e
letItCurl <rolandlopez.developer@gmail.com>
2024-05-05 17:37:33 +0700

    adding test for stream from helper

--- test/controllers/streams_controller_test.rb
+++ test/controllers/streams_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class TurboClone::StreamsHelperTest < ActionDispatch::IntegrationTest
+class TurboClone::StreamsControllerHelperTest < ActionDispatch::IntegrationTest
   test "create with respond to html" do
     post articles_path, params: { article: { content: "something" } }
     assert_redirected_to articles_path
---
+++ test/helpers/stream_helper_test.rb
@@ -0,0 +1,21 @@
+# 0032591256
+require "test_helper"
+
+class TurboClone::StreamsHelperTest < ActionView::TestCase
+  test "with a string streamable" do
+    assert_dom_equal %(<turbo-cable-stream-source channel="TurboClone::StreamsChannel" signed-stream-name="articles"></turbo-cable-stream-source>),
+    turbo_stream_from("articles")
+  end
+
+  test "with a model streamable" do
+    article = Article.new(id: 1)
+    assert_dom_equal %(<turbo-cable-stream-source channel="TurboClone::StreamsChannel" signed-stream-name="article_1"></turbo-cable-stream-source>),
+    turbo_stream_from(article)
+  end
+
+  test "with a mutliple streamable" do
+    article = Article.new(id: 1)
+    assert_dom_equal %(<turbo-cable-stream-source channel="TurboClone::StreamsChannel" signed-stream-name="articles:article_1"></turbo-cable-stream-source>),
+    turbo_stream_from("articles", article)
+  end
+end
